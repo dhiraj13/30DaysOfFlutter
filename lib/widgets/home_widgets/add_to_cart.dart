04fa69bb15e294862f7951878f7010beaff2c26c
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-09-17 10:28:16 +0545

    day 26 completed

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -4,21 +4,17 @@ import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class AddToCart extends StatefulWidget {
+class AddToCart extends StatelessWidget {
   final Item catalog;
-  const AddToCart({
+  AddToCart({
     Key? key,
     required this.catalog,
   }) : super(key: key);
-  @override
-  State<AddToCart> createState() => _AddToCartState();
-}
 
-class _AddToCartState extends State<AddToCart> {
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
+    bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         isInCart = isInCart.toggle();
@@ -27,8 +23,8 @@ class _AddToCartState extends State<AddToCart> {
           final _cart = CartModel();
 
           _cart.catalog = _catalog;
-          _cart.add(widget.catalog);
-          setState(() {});
+          _cart.add(catalog);
+          // setState(() {});
         }
       },
       style: ButtonStyle(
